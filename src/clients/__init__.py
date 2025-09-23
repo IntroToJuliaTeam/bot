9b72706a9c86e9ba2293df6a5ee10ad6548bdad8
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-09-23 16:36:08 +0500

    fix: work in microservises mode

--- src/clients/__init__.py
+++ src/clients/__init__.py
@@ -1,9 +1,21 @@
 import os
+from urllib.parse import urljoin
 
-import requests
 from dotenv import load_dotenv
+from requests import Session
 
-API_CLIENT = requests.Session()
+
+class LiveServerSession(Session):
+    def __init__(self, base_url=None):
+        super().__init__()
+        self.base_url = base_url
+
+    def request(self, method, url, *args, **kwargs):
+        joined_url = urljoin(self.base_url, url)
+        return super().request(method, joined_url, *args, **kwargs)
+
+
+API_CLIENT = LiveServerSession("http://localhost:8080")
 
 load_dotenv()
 
